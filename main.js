0ef23a97522c319f23c46845d37db8da718a58de
Daryl Djelou <daryl.djelou@gmail.com>
2022-12-01 22:16:05 +0100

    feat(condom animation:js script to animate the condom on keypress event

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.css'
+var id = null;
+var pos = 0;
+var width = 50
+document.querySelector("body").addEventListener("keypress", frame)
+function frame(event) {
+  var elem = document.getElementById("put_condom");
+    width ++;
+    elem.style.width = width + 'px';
+}
