1833d97b67cefccf3dcbcc4b71d618375a248cee
mezz <mohamed.mazouz@epitech.eu>
2022-12-02 05:08:30 +0100

    feat:(game) adding unrollable condom

---
+++ condom.js
@@ -0,0 +1,5 @@
+export default class condom {
+    constructor() {
+        this.state = "packed"
+    }
+}
--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 import './style.css'
 var id = null;
+var mousePosition;
 var pos = 0;
+var offset = [0,0];
+var isUnpackaged = 0;
 var width = 50;
-var is_down = false;
+var isDown = false;
 var condoms = document.getElementsByClassName("package_condom")
+var condom_parts = document.getElementsByClassName("condom");
+var cucumber = document.getElementById("cucumber");
+var condoms_width = 200; 
+var cucumberPosition = {
+  x: cucumber.getBoundingClientRect().left,
+  y: cucumber.getBoundingClientRect().y,
+}
+
+function check_collision(element) {
+  if (cucumber.getBoundingClientRect().x - element.getBoundingClientRect().x < 30 
+  && Math.abs(cucumber.getBoundingClientRect().y - element.getBoundingClientRect().y) < 50)
+    return 1;
+  return 0;
+}
 
 document.querySelector("body").addEventListener("keypress", frame)
 
@@ -15,7 +32,46 @@ function frame(event) {
 
 Array.from(condoms).forEach(element => {
   element.addEventListener('click', () => {
+    isUnpackaged = 1;
     element.style.backgroundImage = "url('unpackaged_condom.svg')";
     element.style.cursor = "url('pinch.cur'), pointer"
-  })  
+  })
 })
+
+Array.from(condoms).forEach(element => {
+  element.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        element.offsetLeft - e.clientX,
+        element.offsetTop - e.clientY
+    ];
+  }, true);
+})
+
+Array.from(condom_parts).forEach(part => {
+  part.style.visibility = "hidden"
+})
+
+Array.from(condoms).forEach(element => {
+  element.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (!isUnpackaged || !isDown)
+      return;
+    if (check_collision(element)) {
+      Array.from(condom_parts).forEach(part => {
+        part.style.visibility = "visible"
+      })
+      element.style.visibility = "hidden"
+    }
+    mousePosition = {
+      x : event.clientX,
+      y : event.clientY,
+    };
+    element.style.left = (mousePosition.x + offset[0]) + 'px';
+    element.style.top  = (mousePosition.y + offset[1]) + 'px';
+  }, true);
+})
+
+document.addEventListener('mouseup', function() {
+  isDown = false;
+}, true);
