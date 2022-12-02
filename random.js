57f342b31d9d8bb3b77253eff23776f2e23f2353
Daryl Djelou <daryl.djelou@gmail.com>
2022-12-02 02:05:39 +0100

    feat(random sentence):set random sentence inside div named 'LOL'

---
+++ random.js
@@ -0,0 +1,17 @@
+var Sentences = ["La fellation c'est bien mais avec preservatif c'est mieux",
+"Se faire dépister! Pour profiter de la meilleure expèrience sexuelle",
+"Les moustiques sont justes nuisibles",
+"Sortir non couvert c'est s'assurer une mort fatale",
+"Le dépistage ? c'est gratuit nan ?",
+"Pensez à vous avant votre partenaire",
+"29% des personnes seropositives sont dépistées à un stade avancé de l'infection",
+"Parlez à vos partenaires",
+"protègez vos muqueuses",
+"Vous n'êtes pas surs ? Contactez la ligne Azur",
+"Le sang la principale cause du Sida",
+"À l'image du comcombre sortez couverts",
+"cuCUMber"]
+var i = Math.floor(Math.random() * 12);
+console.log(i)
+let sentence = Sentences[i];
+document.getElementById("LOL").innerText = sentence
