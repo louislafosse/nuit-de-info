c12c22848ea43d397f77f8d809f8281f6cd9d4b5
mezz <mohamed.mazouz@epitech.eu>
2022-12-02 01:31:07 +0100

    feat(game): cursor depending on condom state

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 import './style.css'
 var id = null;
 var pos = 0;
-var width = 50
+var width = 50;
+var is_down = false;
+var condoms = document.getElementsByClassName("package_condom")
+
 document.querySelector("body").addEventListener("keypress", frame)
+
 function frame(event) {
   var elem = document.getElementById("put_condom");
     width ++;
     elem.style.width = width + 'px';
 }
-function cursor_change() {
-    cursor: url("public/finger_cut.png")
-}
+
+Array.from(condoms).forEach(element => {
+  element.addEventListener('click', () => {
+    element.style.backgroundImage = "url('unpackaged_condom.svg')";
+    element.style.cursor = "url('pinch.cur'), pointer"
+  })  
+})
