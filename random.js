a32fc6cea6035e90b386edc03eb9116d22df20c3
Daryl Djelou <daryl.djelou@gmail.com>
2022-12-02 05:20:48 +0100

    feat(full prevention text added)

--- random.js
+++ random.js
@@ -1,16 +1,16 @@
 var Sentences = ["La fellation c'est bien mais avec preservatif c'est mieux",
-"Se faire dépister! Pour profiter de la meilleure expèrience sexuelle",
-"Les moustiques sont justes nuisibles",
-"Sortir non couvert c'est s'assurer une mort fatale",
-"Le dépistage ? c'est gratuit nan ?",
-"Pensez à vous avant votre partenaire",
-"29% des personnes seropositives sont dépistées à un stade avancé de l'infection",
-"Parlez à vos partenaires",
-"protègez vos muqueuses",
-"Vous n'êtes pas surs ? Contactez la ligne Azur",
-"Le sang la principale cause du Sida",
-"À l'image du comcombre sortez couverts",
-"cuCUMber"]
+"Se faire dépister! Pour profiter de la meilleure expèrience sexuelle\nLes MST peuvent être transmises lors d’une fellation sans protection. Le VIH peut être transmis lorsque l’une des 2 personnes est séropositive et qu’une plaie ouverte soit présente dans la bouche.",
+"Les moustiques sont justes nuisibles\nContrairement à ce que l’on pense, les moustiques sont incapables de transmettre le VIH 	puisque ce dernier ne peut pas se reproduire dans les glandes.",
+"Sortir non couvert c'est s'assurer une mort fatale\nLes effets visibles du VIH ne le sont que lors de la phase terminale de la maladie et cette phase conduit à une mort certaine au bout d’un an. Évitez qu’il soit trop tard pour vous dépister.",
+"Le dépistage ? c'est gratuit nan ?\nLe dépistage est gratuit dans tous les CEGIDD (centre gratuit d’information, de dépistage et de diagnostic), les laboratoires d’analyses médicales et les TROD (locaux associatifs).",
+"Pensez à vous avant votre partenaire\n",
+"29% des personnes seropositives sont dépistées à un stade avancé de l'infection\n",
+"Parlez à vos partenaires\n",
+"protègez vos muqueuses\n",
+"Vous n'êtes pas surs ? Contactez la ligne Azur\nLa ligne azur est un numéro que vous pouvez appeler si vous possédez des questions ou si vous vous sentez mal, ils ne sont pas là pour vous juger mais pour vous écouter et vous conseiller. Joignez-les aux 0 810 20 30 40.",
+"Le sang la principale cause du Sida\n29% des personnes atteintes du VIH découvrent leur séropositivité à un stade avancé de l'infection ",
+"À l'image du comcombre sortez couverts\n",
+"cuCUMber\nCUM"]
 var i = Math.floor(Math.random() * 12);
 console.log(i)
 let sentence = Sentences[i];
